64e1acd8a68ef9fb4208b93c53da8e2e9a0fbf99
kitano kaito <af16033@shibaura-it.ac.jp>
2019-10-16 13:39:42 +0900

    最初

---
+++ BookMotion/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  BookMotion
+//
+//  Created by 北野めぐみ on 2019/10/16.
+//  Copyright © 2019年 kitano kaito. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ BookMotion/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  BookMotion
+//
+//  Created by 北野めぐみ on 2019/10/16.
+//  Copyright © 2019年 kitano kaito. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
